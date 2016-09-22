fb6937dc128400086e9986655f743a46b9d04ac4
Joey <bevilacquajoey@gmail.com>
2016-09-22 16:24:41 +0200

    Firebase: improve notification
    * built it locally, so we can show a proper icon instead of launcher one and customize it later
    * Subscribe to address-specific topic
    
    Signed-off-by: Joey <bevilacquajoey@gmail.com>

--- app/src/main/java/it/liceoarzignano/bold/firebase/BoldInstanceIdService.java
+++ app/src/main/java/it/liceoarzignano/bold/firebase/BoldInstanceIdService.java
@@ -1,16 +1,52 @@
 package it.liceoarzignano.bold.firebase;
 
+import android.content.Context;
+
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 import com.google.firebase.messaging.FirebaseMessaging;
 
+import it.liceoarzignano.bold.BoldApp;
+import it.liceoarzignano.bold.Utils;
+
 
 public class BoldInstanceIdService extends FirebaseInstanceIdService {
-    private static final String FRIENDLY_ENGAGE_TOPIC = "friendly_engage";
+    private static final String ADDR1_TOPIC = "Scientifico";
+    private static final String ADDR2_TOPIC = "ScApplicate";
+    private static final String ADDR3_TOPIC = "Linguistico";
+    private static final String ADDR4_TOPIC = "ScUmane";
+    private static final String ADDR5_TOPIC = "Economico";
+    private static final String ADDR6_TOPIC = "Docente";
 
     @Override
     public void onTokenRefresh() {
         FirebaseInstanceId.getInstance().getToken();
-        FirebaseMessaging.getInstance().subscribeToTopic(FRIENDLY_ENGAGE_TOPIC);
+        Context mContext = BoldApp.getBoldContext();
+        String mTopic;
+        if (Utils.isTeacher(mContext)) {
+            mTopic = ADDR6_TOPIC;
+        } else {
+            switch (Utils.getAddress(mContext)) {
+                case "1":
+                    mTopic = ADDR1_TOPIC;
+                    break;
+                case "2":
+                    mTopic = ADDR2_TOPIC;
+                    break;
+                case "3":
+                    mTopic = ADDR3_TOPIC;
+                    break;
+                case "4":
+                    mTopic = ADDR4_TOPIC;
+                    break;
+                case "5":
+                    mTopic = ADDR5_TOPIC;
+                    break;
+                default:
+                    mTopic = ADDR6_TOPIC;
+                    break;
+            }
+        }
+        FirebaseMessaging.getInstance().subscribeToTopic(mTopic);
     }
 }
--- app/src/main/java/it/liceoarzignano/bold/firebase/BoldMessagingService.java
+++ app/src/main/java/it/liceoarzignano/bold/firebase/BoldMessagingService.java
@@ -1,10 +1,17 @@
 package it.liceoarzignano.bold.firebase;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.compat.BuildConfig;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import it.liceoarzignano.bold.MainActivity;
+import it.liceoarzignano.bold.R;
 
 public class BoldMessagingService extends FirebaseMessagingService {
     private static final String TAG = "BoldFireBase";
@@ -16,5 +23,21 @@ public class BoldMessagingService extends FirebaseMessagingService {
             Log.d(TAG, "FCM Notification Message: " + remoteMessage.getNotification());
             Log.d(TAG, "FCM Data Message: " + remoteMessage.getData());
         }
+
+        Intent mIntent = new Intent(this, MainActivity.class);
+        mIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent mPendingIntent = PendingIntent.getActivity(this, 0, mIntent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentTitle(remoteMessage.getNotification().getTitle())
+                .setContentText(remoteMessage.getNotification().getBody())
+                .setAutoCancel(false)
+                .setContentIntent(mPendingIntent);
+
+        NotificationManager mManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        mManager.notify(0, mBuilder.build());
     }
 }
